ed51e62c68561d89e2942c80e88cd7b570d1b1c0
FKzenos <144780851+FKzenos@users.noreply.github.com>
2024-09-04 16:08:56 +0200

    test

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site sur les Plantes</title>
+    <link rel="stylesheet" href="gaia2.css">
+</head>
+<body>
+    <header>
+        <h1>Gaia</h1>
+    </header>
+    
+    <main>
+        <div class="bloc-container">
+            <a href="interieur.html" class="bloc">
+                <h2>Guide de premier pas</h2>
+            </a>
+            <a href="Analyseur_plante.php" class="bloc">
+                <h2>Analyser votre plante</h2>
+            </a>
+            <a href="succulentes.html" class="bloc">
+                <h2>News</h2>
+            </a>
+            <a href="aromatiques.html" class="bloc">
+                <h2>a propos</h2>
+            </a>
+        </div>
+    </main>
+    
+    <footer>
+        <p>&copy; 2077 - Gaia</p>
+    </footer>
+</body>
+</html>
